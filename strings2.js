2973654033f575cf9e6a234d509c6f30e6f8bac5
samyuktha495 <antagirisamyuktha@gmail.com>
2024-11-11 12:48:06 +0530

    Add files via upload

---
+++ strings2.js
@@ -0,0 +1,28 @@
+//1.Relace the substring “World!” with “My Dear Friend” ..
+// Write the program for that?
+
+ let str = "Hello World!";
+ console.log(str.replace("World!","My Dear Friend"));
+
+
+// 2.Repeat the “Hungry” for 10 times
+// Write the program for that
+let a="Hungry \n"
+console.log(a.repeat(10));
+
+// 
+
+// 3.Find the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+// 4.Find the string starts with “hi”
+// Write code for that
+let message = "Hello, world!"
+console.log(message.startsWith("hi"));
+
+// 5.Find the string ends with “.pdf”
+// Write code for that
+let fileName = "document.pdf"
+console.log(fileName.endsWith(".pdf"));
+
